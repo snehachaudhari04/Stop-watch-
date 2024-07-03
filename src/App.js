b1fa7c6eca5e15afac5322e8db07dddb33ee2723
Sneha Chaudhari <snehachaudhari042@gmail.com>
2024-07-03 05:22:06 -0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import Colorgenerator from './Component/colorgenerator/colorgenerator';
+
 
 function App() {
+  const[time,setTime]= useState(0);
+  function change(){
+    let temptime=time+1;
+    setTime(temptime)
+  }
+  const Reset=()=>{
+    setTime(0);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   
+  <h1>
+    today time is {time}
+  </h1>
+  <button onClick={change}>increse</button>
+  <button onClick={Reset}>Reset</button>
+  <Colorgenerator color="green"/>
+  
+ 
+ 
+   </>
   );
 }
 
---
+++ src/Component/colorgenerator/colorgenerator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function colorgenerator(color) {
+    
+  return (
+    <>
+    <div style={{height:50, width:50,backgroundColor:{color}}}>
+         <button>ChangeCoLOR</button>
+    </div>
+    </>
+  )
+}
+export default colorgenerator
