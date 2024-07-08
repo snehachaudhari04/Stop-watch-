4eb392f48d666ebcd4ffd59e8a3b41d612136ba1
Sneha Chaudhari <snehachaudhari042@gmail.com>
2024-07-08 01:20:47 -0700

    copy feature added in password generator

--- src/Component/PasswordGenerator.js
+++ src/Component/PasswordGenerator.js
@@ -1,10 +1,12 @@
-import React, { useState  ,useCallback, useEffect} from 'react'
+import React, { useState  ,useCallback, useEffect,useRef} from 'react'
 
  function PasswordGenerator() {
     const [noallowed,setnoAllowed]=useState(false)
     const [charallowed,setcharAllowed]=useState(false)
     const[length,setLength]=useState(8);
     const [password,setPassword]=useState("")
+    // this useRef is ise for copy button useref is use to refer specific elememt
+    const passref= useRef(null)
 
     const passgenerator = useCallback(()=>{
       let pass=""
@@ -13,16 +15,20 @@ import React, { useState  ,useCallback, useEffect} from 'react'
         str+="1234567890"
       }
       if(charallowed){
-        str+="@#$%&*"
+        str+="@#$%&*{(})-+~"
       }
-      for(let i=0;i<length;i++){
+      for(let i=1;i<=length;i++){
         let char = Math.floor((Math.random()*str.length+1))
         pass+= str.charAt(char)
       }
       setPassword(pass)
      
 
-    },[setPassword,setLength,setnoAllowed,setcharAllowed])
+    },[setPassword,length,setnoAllowed,setcharAllowed])
+    function copyclipboard(){
+      passref.current?.select();
+      window.navigator.clipboard.writeText(password)
+    }
     useEffect(()=>{passgenerator()},[length,noallowed,charallowed])
 
     
@@ -30,13 +36,22 @@ import React, { useState  ,useCallback, useEffect} from 'react'
   return (
     <>
     <div style={{ height:300, backgroundColor:'wheat', display:'flex' ,alignItems:'center',justifyContent:'center', flexDirection:'column'}}>
+        <div>
         <input style={{height:50 ,width:350}}
         type='text'
         value={password}
         placeholder='password'
         readOnly
+        ref={passref}
 
         />
+        <button
+         style={{backgroundColor:'blue',height:50  }}
+         onClick={copyclipboard}
+         >Copy
+         </button>
+
+        </div>
         <div>
         Length{length} <input style={{color: 'blue'}}
         type='range'
